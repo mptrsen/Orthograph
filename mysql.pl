74251b6ac976fe28faf628bd0f6cd49c531a9d58
Malte Petersen <mptrsen@uni-bonn.de>
2012-10-07 23:59:24 +0200

    Possible solution to the redundancy problem?

--- mysql.pl
+++ mysql.pl
@@ -8,41 +8,44 @@ use Data::Dumper;
 my $count    = 0;
 my $data     = { };
 my $db       = 'orthograph';
-my $dbpwd    = 'mpetersen';
+my $dbpwd    = 'malty';
 my $dbserver = 'localhost';
-my $dbuser   = 'mpetersen';
-my $out = { };
+my $dbuser   = 'malty';
+my $fuzzy    = 0;
+my $out      = { };
 my $sort_by  = 'blasteval';
-my @reftaxa  = ('AAEGY');
+my $strict   = 1;
+my @reftaxa  = qw(AAEGY CFLOR ISCAP);
 
 #--------------------------------------------------
-# get query
+# this query selects all orthology candidates for which
+# the reciprocal search also hit something good
 #-------------------------------------------------- 
 my $query = "
 SELECT DISTINCT
-	o_hmmsearch.target           AS transcript,
-	o_orthologs.ortholog_gene_id AS orthoid,
-	o_hmmsearch.evalue           AS hmm_eval,
-	o_blast.target               AS blast_target,
-	o_blast.evalue               AS blast_eval,
-	o_taxa.name                  AS reftax
-FROM o_aaseqs
-INNER JOIN o_taxa
-	ON o_aaseqs.taxid            = o_taxa.id
-INNER JOIN o_blast
-	ON o_aaseqs.id               = o_blast.target
-INNER JOIN o_hmmsearch
-	ON o_blast.query             = o_hmmsearch.target
-INNER JOIN o_ests
-	ON o_hmmsearch.target        = o_ests.digest
-INNER JOIN o_orthologs
-	ON o_hmmsearch.query         = o_orthologs.ortholog_gene_id
-INNER JOIN o_sequence_pairs
-	ON o_orthologs.sequence_pair = o_sequence_pairs.id
-INNER JOIN o_set_details
-	ON o_set_details.name        = 'mosq'
-WHERE o_ests.spec              = 'Mengenilla'
-ORDER BY o_hmmsearch.evalue
+	orthograph_orthologs.ortholog_gene_id AS orthoid,
+	orthograph_hmmsearch.target           AS transcript,
+	orthograph_hmmsearch.evalue           AS hmm_eval,
+	orthograph_blast.target               AS blast_target,
+	orthograph_blast.evalue               AS blast_eval,
+	orthograph_taxa.name                  AS reftax
+FROM orthograph_aaseqs
+INNER JOIN orthograph_taxa
+	ON orthograph_aaseqs.taxid            = orthograph_taxa.id
+INNER JOIN orthograph_blast
+	ON orthograph_aaseqs.id               = orthograph_blast.target
+INNER JOIN orthograph_hmmsearch
+	ON orthograph_blast.query             = orthograph_hmmsearch.target
+INNER JOIN orthograph_ests
+	ON orthograph_hmmsearch.target        = orthograph_ests.digest
+INNER JOIN orthograph_orthologs
+	ON orthograph_hmmsearch.query         = orthograph_orthologs.ortholog_gene_id
+INNER JOIN orthograph_sequence_pairs
+	ON orthograph_orthologs.sequence_pair = orthograph_sequence_pairs.id
+INNER JOIN orthograph_set_details
+	ON orthograph_set_details.name        = 'alltaxa'
+WHERE orthograph_ests.spec              = 'Mengenilla'
+ORDER BY orthograph_hmmsearch.evalue
 ";
 
 # get data, store in hash->array->hash
@@ -51,7 +54,7 @@ my $sql = $dbh->prepare($query);
 $sql->execute();
 while (my $line = $sql->fetchrow_arrayref()) {
 	push @{$$data{$$line[0]}}, {
-		'orthoid'     => $$line[1],
+		'digest'      => $$line[1],
 		'hmmeval'     => $$line[2],
 		'blasttarget' => $$line[3],
 		'blasteval'   => $$line[4],
@@ -62,45 +65,52 @@ while (my $line = $sql->fetchrow_arrayref()) {
 $dbh->disconnect;
 
 # sort by blast evalue or hmmsearch evalue or the number of your mom's chest hairs
-foreach my $est (keys %$data) {
-	@{$$data{$est}} = sort {
+foreach my $eog (keys %$data) {
+	@{$$data{$eog}} = sort {
 		$$a{$sort_by} <=> $$b{$sort_by};
-	} @{$$data{$est}};
+	} @{$$data{$eog}};
 }
 
 # output each eog with the correct transcript
 # each transcript shall be assigned to only one ortholog group
-foreach my $est (keys %$data) {
+foreach my $eog (keys %$data) {
 	# for all hits, see if they are in the reftaxa list
-	for my $i (0..$#{$$data{$est}}) {
+	REFTAXON:
+	for my $i (0..$#{$$data{$eog}}) {
 		# is this reftaxon in our list?
-		if ( grep /$$data{$est}[$i]{'reftaxon'}/ , @reftaxa ) {
+		if ( grep /$$data{$eog}[$i]{'reftaxon'}/ , @reftaxa ) {
 			# ok it's there
-			# take the largest (hmm|blast) evalue
+			# take the best (hmm|blast) evalue
 			# using a hash makes sure each ortholog group gets only one transcript
-			if ( defined $$out{$$data{$est}[$i]{'orthoid'}} ) {
-				if ( $$data{$est}[$i]{$sort_by} < $$out{$$data{$est}[$i]{'orthoid'}}{$sort_by} ) {
-					$$out{$$data{$est}[$i]{'orthoid'}} = {
-						'est'       => $est,
-						'hmmeval'   => $$data{$est}[$i]{'hmmeval'},
-						'blasteval' => $$data{$est}[$i]{'blasteval'},
-						'reftaxon'  => $$data{$est}[$i]{'reftaxon'},
+			if ( defined $$out{$$data{$eog}[$i]{'digest'}} ) {
+				if ( $$data{$eog}[$i]{$sort_by} < $$out{$$data{$eog}[$i]{'digest'}}{$sort_by} ) {
+					$$out{$$data{$eog}[$i]{'digest'}} = {
+						'eog'       => $eog,
+						'hmmeval'   => $$data{$eog}[$i]{'hmmeval'},
+						'blasteval' => $$data{$eog}[$i]{'blasteval'},
+						'reftaxon'  => $$data{$eog}[$i]{'reftaxon'},
 					};
 				}
+				# is this a strict search, i.e., do we need to hit ALL reftaxa?
+				unless ($strict) { last; }
+				else { next REFTAXON }
 			}
 			else {
-				$$out{$$data{$est}[$i]{'orthoid'}} = {
-					'est'       => $est,
-					'hmmeval'   => $$data{$est}[$i]{'hmmeval'},
-					'blasteval' => $$data{$est}[$i]{'blasteval'},
-					'reftaxon'  => $$data{$est}[$i]{'reftaxon'},
+				$$out{$$data{$eog}[$i]{'digest'}} = {
+					'eog'       => $eog,
+					'hmmeval'   => $$data{$eog}[$i]{'hmmeval'},
+					'blasteval' => $$data{$eog}[$i]{'blasteval'},
+					'reftaxon'  => $$data{$eog}[$i]{'reftaxon'},
 				};
 			}
 		}
 	}
 }
 
-print Dumper($out);
-printf "%d hits %d total\n", scalar keys %$data, $count;
-printf "%d eogs %d total\n", scalar keys %$out, $count;
+foreach my $digest (keys %$out) {
+	printf "%s => %s (eval %1.1e)\n", $$out{$digest}{'eog'}, $digest, $$out{$digest}{'blasteval'};
+}
+
+#print Dumper($out);
+printf "%d hits %d 1:1 hits %d total hits\n", scalar keys %$data, scalar keys %$out, $count;
 
