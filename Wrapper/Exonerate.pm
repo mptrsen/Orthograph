d45cfe728fc44f4750e5dde271177bdeebcfabd7
Malte Petersen <mptrsen@uni-bonn.de>
2012-12-21 15:38:11 +0100

    Working on outputting nucleotide sequences

--- Wrapper/Exonerate.pm
+++ Wrapper/Exonerate.pm
@@ -8,9 +8,10 @@ use IO::File; # object-oriented access to files
 use Carp; # extended dying functions
 use Data::Dumper;
 
-my $verbose = 0;
-my $debug = 0;
-my $outdir = File::Spec->catdir('.');
+my $verbose    = 0;
+my $debug      = 0;
+my $exhaustive = 0;
+my $outdir     = File::Spec->catdir('.');
 my $searchprog = 'exonerate';
 my @searchcmd;
 my $evalue_threshold = 10;
@@ -24,7 +25,7 @@ sub new {
 		'target'     => $target,
 		'resultfile' => '',
 		'hitcount'   => 0,
-	}
+	};
 	bless $self, $class;
 	return $self;
 }
@@ -57,9 +58,22 @@ sub debug {#{{{
   $debug = shift;
 }#}}}
 
+=head2 exhaustive
+
+Sets exhaustive search. Default is FALSE.
+
+=cut
+
+sub exhaustive {
+  my $class = shift;
+  if (ref $class) { confess("Class method called as object method") }
+  unless (scalar @_ == 1) { confess("Usage: Wrapper::Exonerate->exhaustive(1|0)") }
+  $exhaustive = shift;
+}
+
 =head2 outdir
 
-Sets output directory for the hmmsearch output files. Expects a reference to
+Sets output directory for the Exonerate output files. Expects a reference to
 scalar F<pathname>. Defaults to F<.>.
 
 =cut
@@ -73,7 +87,7 @@ sub outdir {#{{{
 
 =head2 searchprog
 
-Sets the HMMsearch program. Expects a string. Defaults to 'F<hmmsearch>'.
+Sets the Exonerate program. Expects a string. Defaults to 'F<exonerate>'.
 
 =cut
 
@@ -86,7 +100,7 @@ sub searchprog {#{{{
 
 =head2 score_threshold
 
-Sets or returns the score threshold to use for the blastp search. Defaults to
+Sets or returns the score threshold to use for the exonerate search. Defaults to
 0 (disabled). Note that e-value and score thresholds are mutually exclusive; if
 you set one, this automatically unsets the other.
 
@@ -102,10 +116,20 @@ sub score_threshold {#{{{
 	$evalue_threshold = 0;
 }#}}}
 
+
+=head1 Object methods
+
+=head2 resultfile
+
+Sets or returns the path to the result file.
+
+=cut
+
 sub resultfile {
 	my $self       = shift;
-	my $resultfile = shift;
-	$self->{'resultfile'} = $resultfile;
+	if    (scalar @_ == 0) { return $self->{'query'}->{'sequence'} }
+	elsif (scalar @_ > 1 ) { confess 'Usage: Wrapper::Exonerate->query_sequence($sequence)', "\n" }
+	$self->{'resultfile'} = shift;
 	return 1;
 }
 
@@ -126,7 +150,8 @@ sub search {
 
 	# the complete command line
 	my $exonerate_cmd = qq( $searchprog --score $self->score_threshold --ryo '$exonerate_ryo' --model $exonerate_model --verbose 0 --showalignment no --showvulgar no $exhaustive $self->query_file $self->target_file 2> /dev/null );
-
+	$self->{'result'} = [ `$exonerate_cmd` ] or confess "Error running exonerate: $!\n";
+	return 1;
 }
 
 sub query_header {
@@ -154,6 +179,7 @@ sub query_sequence {
 	if    (scalar @_ == 0) { return $self->{'query'}->{'sequence'} }
 	elsif (scalar @_ > 1 ) { confess 'Usage: Wrapper::Exonerate->query_sequence($sequence)', "\n" }
 	$self->{'query'}->{'sequence'} = shift @_;
+	return 1;
 }
 
 =head2 target_sequence
@@ -167,12 +193,13 @@ sub target_sequence {
 	if    (scalar @_ == 0) { return $self->{'target'}->{'sequence'} }
 	elsif (scalar @_ > 1 ) { confess 'Usage: Wrapper::Exonerate->target_sequence($sequence)', "\n" }
 	$self->{'target'}->{'sequence'} = shift @_;
+	return 1;
 }
 
 sub query_file {
 	my $self = shift;
 	unless (scalar @_ > 0) {
-		my $tmpfh = File::Temp->new( 'UNLINK' => 0 ) or confess "Fatal: Could not open query file '$tmpfh' for writing: $!\n";
+		my $tmpfh = File::Temp->new( 'UNLINK' => 0 ) or confess "Fatal: Could not open query file for writing: $!\n";
 		printf $tmpfh ">%s\n%s\n", $self->query_header, $self->query_sequence or confess "Fatal: Could not write to query file '$tmpfh': $!\n";
 		$self->{'queryfile'} = $tmpfh;
 		return 1;
@@ -183,7 +210,7 @@ sub query_file {
 sub target_file {
 	my $self = shift;
 	unless (scalar @_ > 0) {
-		my $tmpfh = File::Temp->new( 'UNLINK' => 0 ) or confess "Fatal: Could not open target file '$tmpfh' for writing: $!\n";
+		my $tmpfh = File::Temp->new( 'UNLINK' => 0 ) or confess "Fatal: Could not open target file for writing: $!\n";
 		printf $tmpfh ">%s\n%s\n", $self->target_header, $self->target_sequence or confess "Fatal: Could not write to target file '$tmpfh': $!\n";
 		$self->{'targetfile'} = $tmpfh;
 		return 1;
