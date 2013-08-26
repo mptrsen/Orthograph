458337d0fc4a41b07acfacd935cc5e513f0c9f9e
Malte Petersen <mptrsen@uni-bonn.de>
2013-08-26 15:56:45 +0200

    Started modeling the blast factory

--- Wrapper/Blastp.pm
+++ Wrapper/Blastp.pm
@@ -50,8 +50,9 @@ package Wrapper::Blastp;
 use strict;
 use warnings;
 use File::Basename; # basename of files
-use IO::File; # object-oriented access to files
-use Carp; # extended dying functions
+use File::Temp;     # temporary files
+use IO::File;       # object-oriented access to files
+use Carp;           # extended dying functions
 use Data::Dumper;
 my $verbose          = 0;
 my $debug            = 0;
@@ -59,18 +60,20 @@ my $blastoutdir      = File::Spec->catdir('.');
 my $searchprog       = 'blastp';
 my $makeblastdbprog  = 'makeblastdb';
 my $evalue_threshold = 10;
-my $score_threshold  = 0;
+my $score_threshold  = 1;
 my $max_hits         = 100;
 my $blast_cmd;
 my $num_threads      = 1;
 
 sub new {
-	my ($class, $db) = @_;
+	my ($class) = shift @_;
 
+	unless (@_ % 2 == 0) { confess("Uneven number of arguments, can't create object\n") }
 	my $self = {
-		'db'       => $db,
+		'db'       => undef,
 		'query'    => undef,
 		'hitcount' => undef,
+		'outfile'  => undef,
 	};
 
 	bless($self, $class);
@@ -208,15 +211,78 @@ sub max_hits {#{{{
 
 =head3 db()
 
-Returns the BLAST database that was selected upon creating a new object
+Sets or returns the BLAST database.
+
+Args: scalar string path to BLAST database, or nothing. 
+
+Returns: 1 if called with argument, scalar string path to BLAST database (if set) otherwise.
 
 =cut
 
 sub db {#{{{
 	my $self = shift;
-	unless ($self->{'db'}) { confess("I do not have a BLAST db\n") }
+	if (@_ > 1) { confess("Called with more than one argument\n") }
+	elsif (@_) {
+		$self->{'db'} = shift @_;
+		return 1;
+	}
 	return $self->{'db'};
 }#}}}
+
+=head3 query()
+
+Sets or returns the query.
+
+Args: scalar string query sequence, or scalar string path to query file, or nothing.
+
+Returns: 1 if called with argument, scalar string query (sequence or path to file) otherwise.
+
+=cut
+
+sub query {
+	my $self = shift;
+	if (@_ > 1) { confess("Called with more than one argument\n") }
+	elsif (@_) {
+		$self->{'query'} = shift @_;
+		# if this is a sequence 'object'
+		if ( ref $self->{'query'} and $self->{'query'}->{'header'} and $self->{'query'}->{'sequence'} ) {
+			$self->query_file( fastaify($self->{'query'}->{'header'}, $self->{'query'}->{'sequence'}) );
+		}
+		# if this is a file
+		elsif ( -s $self->{'query'} ) {
+			$self->query_file($self->{'query'});
+		}
+		else {
+			confess("Neither sequence object nor file\n");
+		}
+		return 1;
+	}
+	return $self->{'query'};
+}
+
+=head3 query_file()
+
+Sets or returns the query file.
+
+=cut
+
+sub query_file {
+	my $self = shift;
+	if (scalar @_ == 0) { return $self->{'queryfile'} }
+	else { $self->{'queryfile'} = shift @_ }
+}
+
+=head3 output_file()
+
+Sets or returns the output file.
+
+=cut
+
+sub output_file {
+	my $self = shift;
+	if (scalar @_ == 0) { return $self->{'outputfile'} }
+	else { $self->{'outputfile'} = shift @_ }
+}
 
 =head3 blastp()
 
@@ -228,10 +294,9 @@ the e-value set via evalue_threshold().
 
 sub blastp {#{{{
 	my $self = shift;
-	unless (scalar @_ == 2) { confess("Usage: Wrapper::Blastp->blastp(FILE, OUTFILE)\n") }
-	my $queryfile = shift;
-	my $outfile = shift;
-	my $db = $self->db;
+	my $queryfile = $self->query_file();
+	my $outfile = $self->output_file();
+	my $db = $self->db();
 	# return right away if this search has been conducted before
 	if (-e $outfile) {
 		print STDERR "BLAST output file exists in '$outfile'\n" if $debug;
@@ -241,7 +306,7 @@ sub blastp {#{{{
 	else {
 		print STDERR "BLAST output file does not exist in '$outfile', conducting new search\n" if $debug;
 		# use outfmt 7 for comment lines
-		my @blastcmd = qq($searchprog -outfmt '7 qseqid sseqid evalue bitscore qstart qend' -evalue $evalue_threshold -threshold $score_threshold -max_target_seqs $max_hits -num_threads $num_threads -db $db -query $queryfile -out $outfile);
+		my @blastcmd = qq($searchprog -num_threads $num_threads -outfmt '7 qseqid sseqid evalue bitscore qstart qend' -evalue $evalue_threshold -threshold $score_threshold -max_target_seqs $max_hits -db $db -query $queryfile -out $outfile);
 
 		# do the search or die
 		print STDERR "\n@blastcmd\n\n"
@@ -346,6 +411,17 @@ sub makeblastdb {#{{{
 	my $title = shift or croak('Usage: makeblastdb($infile, $outfile, $title)' . "\n");
 	my @makeblastdbcmd = qw($makeblastdbprog -in $infile -out $outfile -input_type fasta -title $title);
 }#}}}
+
+sub fastaify {
+	my $header = shift;
+	my $sequence = shift;
+	my $fh = File::Temp->new(UNLINK=>1);
+	$fh->unlink_on_destroy(0) if $debug;
+	printf { $fh } ">%s\n%s\n", $header, $sequence;
+	close $fh;
+	print "Wrote '$header' to Fasta file '$fh'\n" if $debug;
+	return $fh;
+}
 
 # return true
 1;
