1590bc118474b762080098321a30078b75a48db0
Malte Petersen <mptrsen@uni-bonn.de>
2013-11-26 15:32:38 +0100

    the ortholog module shows its strength

--- Orthograph/Ortholog.pm
+++ Orthograph/Ortholog.pm
@@ -9,6 +9,60 @@ sub new {
 	return bless ($self, $class);
 }
 
+sub hmm_target {
+	my $self = shift @_;
+	if (scalar @_ > 0) {
+		$self->{'hmm_target'} = shift @_;
+		return 1;
+	}
+	return $self->{'hmm_target'};
+}
+
+sub hmm_name {
+	my $self = shift @_;
+	if (scalar @_ > 0) {
+		$self->{'hmm_name'} = shift @_;
+		return 1;
+	}
+	return $self->{'hmm_name'};
+}
+
+sub hmm_start {
+	my $self = shift @_;
+	if (scalar @_ > 0) {
+		$self->{'hmm_start'} = shift @_;
+		return 1;
+	}
+	return $self->{'hmm_start'};
+}
+
+sub hmm_end {
+	my $self = shift @_;
+	if (scalar @_ > 0) {
+		$self->{'hmm_end'} = shift @_;
+		return 1;
+	}
+	return $self->{'hmm_end'};
+}
+
+sub env_start {
+	my $self = shift @_;
+	if (scalar @_ > 0) {
+		$self->{'env_start'} = shift @_;
+		return 1;
+	}
+	return $self->{'env_start'};
+}
+
+sub env_end {
+	my $self = shift @_;
+	if (scalar @_ > 0) {
+		$self->{'env_end'} = shift @_;
+		return 1;
+	}
+	return $self->{'env_end'};
+}
+
 sub blast_target {
 	my $self = shift @_;
 	if (scalar @_ > 0) {
